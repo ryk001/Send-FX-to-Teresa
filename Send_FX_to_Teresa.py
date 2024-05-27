b8af1284668c296cf92d557300b27d0f41ead2b3
Ryan Kan, 叡 <48592504+ryk001@users.noreply.github.com>
2024-05-28 00:12:07 +0800

    Update Send_FX_to_Teresa.py

--- Send_FX_to_Teresa.py
+++ Send_FX_to_Teresa.py
@@ -1,5 +1,6 @@
 # load package
 from google_currency import convert
+from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import os
@@ -8,7 +9,9 @@ import os
 usd_to_jpy = convert('usd', 'jpy', 1)[40:45]
 usd_to_ntd = convert('usd', 'twd', 1)[40:45]
 ntd_to_jpy = convert('twd', 'jpy', 1)[40:44]
-Cathay_usd = str(pd.read_html("https://www.cathaybk.com.tw/cathaybk/personal/deposit-exchange/rate/currency-billboard/?p=furtheradvance&p=furtheradvance&p=treeloan&p=mortgage&p=employeeloan&dev=mobile&CUB_SRC=GOOGLE&CUB_CHL1=URL&CUB_CHL2=01&MA_TK=DB197&CUB_DT=20190701&utm_source=Cathay_officialwebsite&utm_medium=loan_homemenu&utm_campaign=19Q3_alwayson&utm_content=1&utm_term=1&DEV=MOBILE")[0]['銀行賣出  Bank sell'][0])
+
+r = requests.get('https://www.cathaybk.com.tw/cathaybk/personal/product/deposit/currency-billboard')
+cathay_usd = BeautifulSoup(r.text, "lxml").find_all('tr')[1].find_all('td')[2].find('div').text
 
 # send message
 def lineNotifyMessage(token, msg):
@@ -20,6 +23,12 @@ def lineNotifyMessage(token, msg):
     return r.status_code
 
 # wrap_up
-message = str('\n' + '國泰美金兌台幣: ' + Cathay_usd + '\n' + '\n' + 'USD to JPY: ' + usd_to_jpy + '\n' + 'USD to NTD: ' + usd_to_ntd + '\n' + 'NTD to JPY: '+ ntd_to_jpy + '\n' + 'source: Google currency')
+message =(
+    f'\n國泰美金兌台幣: {cathay_usd}\n\n'
+    f'USD to JPY: {usd_to_jpy}\n'
+    f'USD to NTD: {usd_to_ntd}\n'
+    f'NTD to JPY: {ntd_to_jpy}\n'
+    'source: Google currency'
+)
 token = os.environ['LINE_NOTIFY_TOKEN']
 lineNotifyMessage(token, message)
